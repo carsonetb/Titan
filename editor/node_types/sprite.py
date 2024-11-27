c9f0fa0b0bd125284d1d940a1154d83835e4a364
Carson Bates <blackhole243243@gmail.com>
2024-11-26 19:52:32 -0800

    Started working on support for shaders for sprites

--- editor/node_types/sprite.py
+++ editor/node_types/sprite.py
@@ -13,6 +13,9 @@ class Sprite(Position):
         self.node_type = "Sprite"
         self.image_width = None
         self.image_height = None
+        self.shader_path = ""
+        self.shader = None
+        self.render_target = None
 
     def editor_update(self, origin_offset):
         super().editor_update(origin_offset)
@@ -22,15 +25,37 @@ class Sprite(Position):
         if self.sprite_path:
             raylib.DrawTexturePro(self.image, [0.0, 0.0, self.image_width, self.image_height], [int(offset_position.x), int(offset_position.y), self.image_width * self.scale.x, self.image_height * self.scale.y], [self.image_width / 2 * self.scale.x, self.image_height / 2 * self.scale.y], self.rotation_degrees, raylib.WHITE)
 
+    def build_textures(self):
+        if not self.sprite_path:
+            return
+
+        if self.image_width * self.scale.x != self.render_target.texture.width or self.image_width * self.scale.y != self.render_target.texture.height:
+            self.load_render_target()
+
+        raylib.BeginTextureMode(self.render_target)
+        raylib.ClearBackground(raylib.WHITE)
+
+        dimensions = [0.0, 0.0, self.image_width, self.image_height]
+        scaled_dimensions = Vector2(self.image_width * self.scale.x, self.image_height * self.scale.y)
+        raylib.DrawTexturePro(self.image, dimensions, [0, 0, scaled_dimensions.x, scaled_dimensions.y], [0, 0], 0, raylib.WHITE)
+
+        raylib.EndTextureMode()
+
     def game_update(self):
         super().game_update()
 
         # Draw sprite to screen.
         if self.sprite_path:
+            if self.shader:
+                raylib.BeginShaderMode()
+            
             dimensions = [0.0, 0.0, self.image_width, self.image_height]
-            scaled_position = Vector2(int(self.global_position.x - (self.image_width * self.scale.x) / 2), int(self.global_position.y - (self.image_height * self.scale.y) / 2))
             scaled_dimensions = Vector2(self.image_width * self.scale.x, self.image_height * self.scale.y)
-            raylib.DrawTexturePro(self.image, dimensions, [scaled_position.x, scaled_position.y, scaled_dimensions.x, scaled_dimensions.y], [0, 0], self.rotation_degrees, raylib.WHITE)
+            scaled_position = Vector2(int(self.global_position.x - (self.image_width * self.scale.x) / 2), int(self.global_position.y - (self.image_height * self.scale.y) / 2))
+            raylib.DrawTexturePro(self.render_target.texture, dimensions, [scaled_position.x, scaled_position.y, scaled_dimensions.x, scaled_dimensions.y], [0, 0], self.rotation_degrees, raylib.WHITE)
+
+            if self.shader:
+                raylib.EndShaderMode()
 
     def generate_engine_interactable(self):
         return SpriteEngineInteractable(
@@ -51,7 +76,15 @@ class Sprite(Position):
         self.image = raylib.LoadTextureFromImage(image)
         self.image_width = image.width
         self.image_height = image.height
+        self.load_render_target()
         raylib.UnloadImage(image)
+    
+    def load_render_target(self):
+        self.render_target = raylib.LoadRenderTexture(self.image_width * self.scale.y, self.image_height * self.scale.y)
+    
+    def load_shader(self, path):
+        self.shader_path = path
+        self.shader = raylib.LoadShader(0, path)
 
     def get_properties_dict(self):
         return {
@@ -64,12 +97,18 @@ class Sprite(Position):
             "scale_x": self.scale.x,
             "scale_y": self.scale.y,
             "rotation": self.rotation,
-            "sprite_path": self.sprite_path if self.sprite_path else "None"
+            "sprite_path": self.sprite_path if self.sprite_path else "None",
+            "shader_path": self.shader_path if self.shader_path else "None",
         }
     
     def load_self(self, node):
         super().load_self(node)
     
         self.sprite_path = node["sprite_path"] if node["sprite_path"] != "None" else None
+        self.shader_path = node["shader_path"] if node["shader_path"] != "None" else None
+
         if self.sprite_path:
             self.set_texture(self.sprite_path)
+        if self.shader_path:
+            self.load_shader(self.shader_path)
+        
